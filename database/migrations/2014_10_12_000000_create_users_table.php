fa82e7ae07aa8357a4be3307bec50e4cff4584a6
Nadeem Nagpurwala <nagpurwalanadeem@gmail.com>
2022-01-30 19:34:49 +0530

    Add migration for users and books table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,14 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->bigInteger('mobile')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('age')->default('0');
+            $table->enum('gender', ['m', 'f', 'o']);
+            $table->string('city');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
